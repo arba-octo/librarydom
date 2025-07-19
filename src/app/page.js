df6c66fc1bec6e21be77d826d7d02dca72a41329
arba-octo <nevarus@yandex.ru>
2025-07-19 21:12:58 +0300

    изменен путь к books в page (Home)

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HomeClient from "@/components/HomeClient";
 // Серверный компонент (SSR)
 export default async function Home() {
     // Лучше явно указывать протокол и порт, если API локальный
-    const res = await fetch("http://localhost:3000/api/v1/books", { cache: "no-store" });
+    const res = await fetch("/api/v1/books", { cache: "no-store" });
     const books = await res.json();
 
     return <HomeClient books={books} />;
