abdcb63194467761959db9daaa26c2139287fbf0
arba-octo <nevarus@yandex.ru>
2025-07-21 15:52:16 +0300

    исправлен: process.env.API_URL

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,17 @@ import HomeClient from "@/components/HomeClient";
 // Серверный компонент (SSR)
 export default async function Home() {
     const host =
-        typeof window === "undefined" && process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
+        typeof window === "undefined" && process.env.API_URL
+            ? process.env.API_URL
             : "";
-    const res = await fetch(`${host}/api/v1/books`, { cache: "no-store" });
-    if (!res.ok) {
-        throw new Error('Failed to fetch');
-    };
-    const books = await res.json();
+    let books = []; // Заглушка
+    try {
+        const res = await fetch(`${host}/api/v1/books`, { cache: "no-store" });
+        if (!res.ok) { throw new Error('Failed to fetch') }
+        books = await res.json();
+    }
+    catch (error) { console.error('Fetch error:', error) }
+    console.log('books (выгрузка с сервера в Home) = ', books);
 
     return <HomeClient books={books} />;
 }
