87936eaba58471632ff22c46b776105f525c7840
arba-octo <nevarus@yandex.ru>
2025-08-03 12:51:33 +0300

    br-9: Исправлено CSS в BookCard

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 import HomeClient from "@/components/HomeClient";
 import { getBooks } from "@/lib/getBooks";
 
-// Серверный компонент (SSR)
+// Серверный компонент (SSR) - загружает книги из базы и передает в props (в глобальный стейт их сразу передать нельзя так как это серверный компонент)
 export default async function Home() {
     const host = process.env.API_URL || "http://localhost:3000";
-    console.log('host = ', host);
     let books = []; // Заглушка
     try {
         books = await getBooks();
     }
     catch (error) { console.error('Ошика в Fetch (catch) в компоненте Home:', error) }
-    console.log('books (загрузились с сервера в Home)  = ', books);
     return <HomeClient books={books} />;
 }
--- src/components/HomeClient.js
+++ src/components/HomeClient.js
@@ -1,10 +1,20 @@
+'use client';
+
 import Header from "@/components/Header/Header";
 import SideBar from "@/components/SideBar/SideBar";
 import LineSeparate from "@/components/LineSeparate";
 import Books from "@/components/Books/Books";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import {setBooks} from "@/features/books-slice";
+import {setFilteredBooks} from "@/features/search/search-slice";
 
 export default function HomeClient({ books }) {
-
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(setBooks(books));
+        dispatch(setFilteredBooks(books));
+    }, [books, dispatch])
 
     return (
         <div>
